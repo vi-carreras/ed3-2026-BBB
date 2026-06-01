1c0323627b3bbd8a6efac41e6e67f83aca86f2ff
vi-carreras <victoria.carreras@mi.unc.edu.ar>
2026-06-01 18:35:55 -0300

    Add new functions to general library

--- include/game_lib/general.h
+++ include/game_lib/general.h
@@ -2,18 +2,28 @@
  * general.h
  *
  *  Created on: 31 may. 2026
- *      Author: 
- */
-
-/*	FALTA:
- * -mensajeLCD();
- * -EscanearTeclado1();
- * -EscanearTeclado2();
+ *      Author:
  */
 
 #ifndef GENERAL_H_
 #define GENERAL_H_
 
-uint8_t EscanearTeclado(void);
+#include "LPC17xx.h"
+#include "lpc17xx_i2c.h"
+
+
+uint8_t EscanearTecladoJ1(void);
+
+uint8_t EscanearTecladoJ2(void);
+
+void mensajeLCD(char* linea1, char* linea2); 
+
+//void reproducirAudio(uint32_t* buffer, uint32_t size);
+
+//void detenerAudio(void);
+
+//void enviarStatsUART(int8_t vict_j1, int8_t vict_j2, uint32_t tiempo_reaccion, uint8_t resultado);
+
+//uint8_t generarTeclaAleatoria(void);
 
 #endif /* GENERAL_H_ */
