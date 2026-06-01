28f89f29ee6d56e4ce807976330206d1b11f04fa
vi-carreras <victoria.carreras@mi.unc.edu.ar>
2026-06-01 17:06:15 -0300

    Add code file to include folder

--- include/game_lib/general.h
+++ include/game_lib/general.h
@@ -2,19 +2,18 @@
  * general.h
  *
  *  Created on: 31 may. 2026
- *      Author: Usuario
+ *      Author: 
+ */
+
+/*	FALTA:
+ * -mensajeLCD();
+ * -EscanearTeclado1();
+ * -EscanearTeclado2();
  */
 
 #ifndef GENERAL_H_
 #define GENERAL_H_
 
-// Función auxiliar para leer la tecla actual basada en los GPIO
-uint8_t LeerTeclaMatricial(void) {
-    // Se lee el registro FIO0PIN, se enmascara y se determina qué columna está en bajo (o alto, según lógica).
-    uint32_t columnas = (LPC_GPIO0->FIOPIN >> 4) & 0x0F;
-    // Retorna un identificador de tecla.
-    // Lógica de mapeo.
-    // return mapeo_tecla;
-}
+uint8_t EscanearTeclado(void);
 
 #endif /* GENERAL_H_ */
